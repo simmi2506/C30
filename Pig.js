10a63f0fdf37cb6a7aa925f2527bf800789bce94
simmi2506 <stynejohn06@gmail.com>
2020-10-12 20:54:48 +0530

    initial commit

--- Pig.js
+++ Pig.js
@@ -4,4 +4,20 @@ class Pig extends BaseClass {
     this.image = loadImage("sprites/enemy.png");
   }
 
+  display(){
+    //step1
+    //when some object touches pig, it's speed increases.
+    console.log(this.body.speed);
+    //step2
+    //if speed is less than 3 then only display the pig
+    if(this.body.speed < 3){
+      super.display();
+    } 
+    //step3
+    //otherwise (that means if speed less than 3) then remove the pig from the world --> means vanish the pig
+    else{
+      World.remove(world, this.body);
+    }   
+  }
+
 };
